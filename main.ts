46697c3cdfc473ca97c03034849373b589d8f158
784179 <784179@dpsk12.net>
2025-01-24 10:59:15 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,39 +1,9 @@
 tiles.setCurrentTilemap(tilemap`level2`)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . c c c c . . . 
-    . . . . . . . c c d d d d c . . 
-    . . . . . c c c c c c d d c . . 
-    . . . c c c 4 4 4 4 d c c c c c 
-    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
-    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
-    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
-    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
-    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
-    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
-    . . f 4 4 1 4 c c 4 4 d f . . . 
-    . . . f d 4 4 4 4 4 4 c f . . . 
-    . . . . f f 4 4 4 4 c d b c . . 
-    . . . . . . f f f f d d d c . . 
-    . . . . . . . . . . c c c . . . 
-    `, SpriteKind.Player)
-let myEnemy = sprites.create(img`
-    .............ccfff..............
-    ...........ccddbcf..............
-    ..........ccddbbf...............
-    ..........fccbbcf...............
-    .....fffffccccccff.........ccc..
-    ...ffbbbbbbbcbbbbcfff....ccbbc..
-    ..fbbbbbbbbcbcbbbbcccff.cdbbc...
-    ffbbbbbbffbbcbcbbbcccccfcdbbf...
-    fbcbbb11ff1bcbbbbbcccccffbbf....
-    fbbb11111111bbbbbcccccccbbcf....
-    .fb11133cc11bbbbcccccccccccf....
-    ..fccc31c111bbbcccccbdbffbbcf...
-    ...fc13c111cbbbfcddddcc..fbbf...
-    ....fccc111fbdbbccdcc.....fbbf..
-    ........ccccfcdbbcc........fff..
-    .............fffff..............
-    `, SpriteKind.Enemy)
-controller.moveSprite(mySprite)
-myEnemy.follow(mySprite)
+let fish = Render.getRenderSpriteInstance()
+let shark = sprites.create(assets.image`myImage1`, SpriteKind.Enemy)
+controller.moveSprite(fish, 100, 100)
+Render.setViewMode(ViewMode.raycastingView)
+tiles.placeOnTile(shark, tiles.getTileLocation(0, 1))
+game.onUpdateInterval(500, function () {
+    scene.followPath(shark, scene.aStar(shark.tilemapLocation(), Render.getRenderSpriteInstance().tilemapLocation()), 100)
+})
